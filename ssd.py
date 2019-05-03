464ced2bdbd5311d75742e5e787afb3d672dc8c6
Arthur <tosterius@yandex.ru>
2019-05-03 20:32:36 +0300

    just a few ssd layers

--- ssd.py
+++ ssd.py
@@ -28,24 +28,38 @@ class SSD:
     def __init_vgg_16_part(self, inputs, scope='vgg_16'):
         with variable_scope.variable_scope(scope, 'vgg_16', [inputs]) as sc:
             end_points_collection = sc.original_name_scope + '_end_points'
-            with slim.arg_scope([layers.conv2d, layers_lib.max_pool2d], outputs_collections=end_points_collection):
-                net = layers_lib.repeat(inputs, 2, layers.conv2d, 64, [3, 3], scope='conv1')
-                net = layers_lib.max_pool2d(net, [2, 2], scope='pool1')
-                net = layers_lib.repeat(net, 2, layers.conv2d, 128, [3, 3], scope='conv2')
-                net = layers_lib.max_pool2d(net, [2, 2], scope='pool2')
-                net = layers_lib.repeat(net, 3, layers.conv2d, 256, [3, 3], scope='conv3')
-                net = layers_lib.max_pool2d(net, [2, 2], scope='pool3')
-                net = layers_lib.repeat(net, 3, layers.conv2d, 512, [3, 3], scope='conv4')
-                net = layers_lib.max_pool2d(net, [2, 2], scope='pool4')
-                net = layers_lib.repeat(net, 3, layers.conv2d, 512, [3, 3], scope='conv5')
-                # net = layers_lib.max_pool2d(net, [2, 2], scope='pool5')
+            with slim.arg_scope([layers.conv2d, slim.max_pool2d], outputs_collections=end_points_collection):
+                net = slim.repeat(inputs, 2, layers.conv2d, 64, [3, 3], scope='conv1')
+                net = slim.max_pool2d(net, [2, 2], scope='pool1')
+                net = slim.repeat(net, 2, layers.conv2d, 128, [3, 3], scope='conv2')
+                net = slim.max_pool2d(net, [2, 2], scope='pool2')
+                net = slim.repeat(net, 3, layers.conv2d, 256, [3, 3], scope='conv3')
+                net = slim.max_pool2d(net, [2, 2], scope='pool3')
+                net = slim.repeat(net, 3, layers.conv2d, 512, [3, 3], scope='conv4')
+                net = slim.max_pool2d(net, [2, 2], scope='pool4')
+                net = slim.repeat(net, 3, layers.conv2d, 512, [3, 3], scope='conv5')
 
                 end_points = utils.convert_collection_to_dict(end_points_collection)
                 self.net = net
                 self.end_points = end_points
 
-    def __init_ssd_300_part(self):
-        self.net = layers_lib.max_pool2d(self.net, [3, 3], stride=1, scope='pool5')
+    def __init_ssd_300_part(self, scope='ssd_300', is_training=True, dropout_keep_prob=0.6):
+        with variable_scope.variable_scope(scope, 'ssd_300', [self.net]) as sc:
+            end_points_collection = sc.original_name_scope + '_end_points'
+            with slim.arg_scope([layers.conv2d, slim.max_pool2d], outputs_collections=end_points_collection):
+                # this is the first difference with VGG
+                self.net = slim.max_pool2d(self.net, [3, 3], stride=1, scope='pool5')
+
+                self.net = slim.conv2d(self.net, 1024, [3, 3], rate=6, scope='conv6')
+                self.net = tf.layers.dropout(self.net, rate=dropout_keep_prob, training=is_training)
+
+                self.net = slim.conv2d(self.net, 1024, [1, 1], scope='conv7')
+                self.net = tf.layers.dropout(self.net, rate=dropout_keep_prob, training=is_training)
+
+                self.net = slim.conv2d(self.net, 256, [1, 1], scope='conv8_1')
+                self.net = slim.conv2d(self.net, 512, [3, 3], stride=2, scope='conv8_2')
+
+
         pass
 
 
