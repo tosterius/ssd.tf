034bad2594d9bd24b224c70cb889e2d029ea1b45
tosterius <tosterius@yandex.ru>
2019-05-26 15:24:27 +0300

    bugfix: voc2007 label dict

--- dataset.py
+++ dataset.py
@@ -11,7 +11,7 @@ class VocDataset:
         self.annotations_root = os.path.join(root_directory, 'Annotations')
         self.images_root = os.path.join(root_directory, 'Images')
         self.label_map = {'background': 0,
-                          'aeroplane': 1, 'bicycle:2''bird': 3, 'boat': 4, 'bottle': 5,
+                          'aeroplane': 1, 'bicycle':2, 'bird': 3, 'boat': 4, 'bottle': 5,
                           'bus': 6, 'car': 7, 'cat': 8, 'chair': 9, 'cow': 10,
                           'diningtable': 11, 'dog': 12, 'horse': 13, 'motorbike': 14, 'person': 15,
                           'pottedplant': 16, 'sheep': 17, 'sofa': 18, 'train': 19, 'tvmonitor': 20}
@@ -51,3 +51,7 @@ class VocDataset:
             labeled_image.objects.append(LabeledObject(label=label, xc=xc, yc=yc, w=w, h=h))
 
         self.data.append(labeled_image)
+
+
+if __name__ == '__main__':
+    ds = VocDataset('/home/arthur/Workspace/projects/github/ssd.tf/VOC2007')
