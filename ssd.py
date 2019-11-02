be05b245cb6becae27b37602e6e0cfe63ab2c97f
Arthur <tosterius@yandex.ru>
2019-11-02 11:48:52 +0300

    experiments with l2 regularization

--- ssd.py
+++ ssd.py
@@ -101,15 +101,15 @@ class SSD:
 
                 self.vgg_end_points = utils.convert_collection_to_dict(end_points_collection)
 
-    def __init_ssd_part(self, scope='ssd_300', is_training=True, dropout_keep_prob=0.6):
+    def __init_ssd_part(self, scope='ssd_300', is_training=True, dropout_keep_prob=0.5):
         with variable_scope.variable_scope(scope, 'ssd_300', [self.net]) as sc:
             end_points_collection = sc.original_name_scope + '_end_points'
-            with slim.arg_scope([slim.conv2d, slim.max_pool2d], outputs_collections=end_points_collection):
-                self.net = slim.max_pool2d(self.net, [3, 3], stride=1, scope='pool5', padding='SAME')
+            self.net = slim.max_pool2d(self.net, [3, 3], stride=1, scope='pool5', padding='SAME')
+            with slim.arg_scope([slim.conv2d, slim.max_pool2d], outputs_collections=end_points_collection,
+                                weights_regularizer=slim.l2_regularizer(0.001)):
 
                 self.net = slim.conv2d(self.net, 1024, [3, 3], rate=6, scope='conv6')
                 self.net = tf.layers.dropout(self.net, rate=dropout_keep_prob, training=is_training)
-
                 self.net = slim.conv2d(self.net, 1024, [1, 1], scope='conv7')
                 self.net = tf.layers.dropout(self.net, rate=dropout_keep_prob, training=is_training)
                 self.feature_maps.append(self.net)
@@ -207,8 +207,10 @@ class SSD:
             self.localization_loss = tf.reduce_mean(localization_loss, name='localization_loss')
 
         with tf.variable_scope('total_loss'):
-            self.l2_loss = tf.multiply(decay, self.l2_norm, name='l2_loss')
-            self.loss = tf.add(self.localization_loss, self.confidence_loss, name='loss')
+            reg_w = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+            l2_det_loss = tf.multiply(decay, self.l2_norm, name='l2_loss')
+            self.l2_loss = l2_det_loss + tf.reduce_sum(reg_w)
+            self.loss = tf.add(self.localization_loss, self.confidence_loss, name='loc_conf_loss')
             self.loss = tf.add(self.loss, self.l2_loss, name='loss')
 
         with tf.variable_scope('optimizer'):
